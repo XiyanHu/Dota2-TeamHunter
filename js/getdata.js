cb5eac77f662d8f4efd3d5aa8a988ec50fb6eabb
XiyanHu <huxiyan927@gmail.com>
2017-06-06 09:55:54 -0700

    Update

--- js/getdata.js
+++ js/getdata.js
@@ -18,12 +18,8 @@ function showMe(team_id,team_name){
 	        	if (xhr.status == 200){
 	        		callback(xhr.response);	
 	        	}else{
-	        		document.getElementById("main_table").innerHTML = "<h4>Ooops! Maybe there is something wrong with API. Wait a minute and try again.</h4>"
-	        	}        
-	        } 
-	    }
-	    xhr.send();
-	}
+	        		document.getElementById("main_table").innerHTML = "<h4>Ooops! Maybe there is something wrong with API. Wait a minute and try again.</h4>"}   } }
+	    xhr.send();}
 	var team_info;
 	httpRequest("https://api.opendota.com/api/proPlayers/", function(result){
 	    team_info = JSON.parse(result);
@@ -31,15 +27,10 @@ function showMe(team_id,team_name){
 	    var player_explain = "Big circle images represent main players and small square images represent reserved players."
 	    teamMemberCode += '<p><span id="player_title">Players  </span><i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="'+ player_explain+'"></i></p>';	    
 	    var numOfLine = 0;
-	    var teamLength = team_info.length;
-		for (var i=0;i<teamLength;i++){		
+		for (var i=0,teamLength = team_info.length;i<teamLength;i++){		
 			if (team_info[i].team_id==test_team_id){		
-				if (numOfLine == 0){
-					teamMemberCode +='<tr>';
-				}
-				numOfLine ++;
-				var avatar;
-				var img_shape;
+				if (numOfLine == 0){teamMemberCode +='<tr>';}
+				numOfLine ++;var avatar;var img_shape;
 				if (team_info[i].is_locked){
 					avatar = team_info[i].avatarmedium;
 					img_shape = "img-circle";
@@ -54,23 +45,17 @@ function showMe(team_id,team_name){
 				teamMemberCode += '</div></td>';
 				if (numOfLine == 5){
 					teamMemberCode += '</tr>';
-					numOfLine = 0;
-				}
-			}
-		}
+					numOfLine = 0;}}}
 	$("#team_member").css('display','block');
 	document.getElementById('team_member').innerHTML = teamMemberCode;
 	$("#loadingTeamInfo").css('display','none'); 
 	$('[data-toggle="tooltip"]').tooltip();
 	});
-	var r;
-	var recentGameUrl = "https://api.opendota.com/api/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0A((player_matches.player_slot%20%3C%20128)%20%3D%20matches.radiant_win)%20win%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.account_id%2C%0Aleagues.name%20leaguename%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0AJOIN%20leagues%20using(leagueid)%0AJOIN%20player_matches%20using(match_id)%0ALEFT%20JOIN%20notable_players%20using(account_id)%0ALEFT%20JOIN%20teams%20using(team_id)%0AJOIN%20heroes%20ON%20player_matches.hero_id%20%3D%20heroes.id%0AWHERE%20TRUE%0AAND%20notable_players.team_id%20%3D%20"+test_team_id+"%0AORDER%20BY%20matches.match_id%20DESC%20NULLS%20LAST%0ALIMIT%2050"
+	var r;var recentGameUrl = "https://api.opendota.com/api/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0A((player_matches.player_slot%20%3C%20128)%20%3D%20matches.radiant_win)%20win%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.account_id%2C%0Aleagues.name%20leaguename%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0AJOIN%20leagues%20using(leagueid)%0AJOIN%20player_matches%20using(match_id)%0ALEFT%20JOIN%20notable_players%20using(account_id)%0ALEFT%20JOIN%20teams%20using(team_id)%0AJOIN%20heroes%20ON%20player_matches.hero_id%20%3D%20heroes.id%0AWHERE%20TRUE%0AAND%20notable_players.team_id%20%3D%20"+test_team_id+"%0AORDER%20BY%20matches.match_id%20DESC%20NULLS%20LAST%0ALIMIT%2050"
 	httpRequest(recentGameUrl, function(result){
-	    r = JSON.parse(result);  
-	    var len1 = r.rowCount;
+	    r = JSON.parse(result);  var len1 = r.rowCount;
 	     if (len1==0){
-	    	document.getElementById("main_table").innerHTML = "<h4>Ooops!It seems your team didn't have games recently.</h4>"
-	    }
+	    	document.getElementById("main_table").innerHTML = "<h4>Ooops!It seems your team didn't have games recently.</h4>"}
 	var tableCode = '';
 	$("#loading").css('display','none');
 	if (localStorage["firstMatchId"] != r.rows[0].match_id){
@@ -82,23 +67,18 @@ function showMe(team_id,team_name){
 	for (var i=0;i<len1;i++){
 		if (i>=1){
 			if(r.rows[i].match_id==r.rows[i-1].match_id){
-				continue;
-			}
-		}
+				continue;}}
 		var detail = new XMLHttpRequest();
 		detail.open("GET","https://api.opendota.com/api/matches/"+r.rows[i].match_id,false);
 		detail.send(); 
 		var detaildata = JSON.parse(detail.response);		
 		if (detaildata.hasOwnProperty("radiant_team")){
-			var rteam = detaildata.radiant_team.name;	
-		}
+			var rteam = detaildata.radiant_team.name;	}
 		if (detaildata.hasOwnProperty("dire_team")){
-			var dteam = detaildata.dire_team.name;	
-		}	
+			var dteam = detaildata.dire_team.name;	}	
 		var starttime = detaildata.start_time;
 		if (rteam!=team_name&&dteam!=team_name){
-			continue;
-		}
+			continue;}
 		var startdate = new Date();
 		startdate.setTime(starttime * 1000);
 		var mainTable = document.getElementById('main_table');
@@ -106,12 +86,7 @@ function showMe(team_id,team_name){
 		var startdateCut = startdate.toLocaleString('en-US',{hour12:false});
 		startdateCut = startdateCut.substring(0,startdateCut.lastIndexOf(':'));
 		if (r.rows[i].win==true){
-			tableCode = tableCode + '<tr class="success" ><td>'+startdateCut+'</td><td>'+r.rows[i].leaguename+'</td><td>'+rteam+' VS '+dteam+'</td><td> WIN </td><td><a href="'+match_detail_url+'" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td></tr>';
-		} 
+			tableCode = tableCode + '<tr class="success" ><td>'+startdateCut+'</td><td>'+r.rows[i].leaguename+'</td><td>'+rteam+' VS '+dteam+'</td><td> WIN </td><td><a href="'+match_detail_url+'" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td></tr>';} 
 		else {
-			tableCode = tableCode + '<tr class="danger" ><td>'+startdateCut+'</td><td>'+r.rows[i].leaguename+'</td><td>'+rteam+' VS '+dteam+'</td><td> LOSE </td><td><a href="'+match_detail_url+'" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></i></a></tr>';	
-		}
-	}
-	mainTable.innerHTML = tableCode;
-	});	
-}
+			tableCode = tableCode + '<tr class="danger" ><td>'+startdateCut+'</td><td>'+r.rows[i].leaguename+'</td><td>'+rteam+' VS '+dteam+'</td><td> LOSE </td><td><a href="'+match_detail_url+'" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></i></a></tr>';	}}
+	mainTable.innerHTML = tableCode;});	}
--- js/transdata.js
+++ js/transdata.js
@@ -4,41 +4,28 @@ $(document).ready(function() {
 				    var xhr = new XMLHttpRequest();
 				    xhr.open("GET", url, true);
 				    xhr.onreadystatechange = function() {
-				        if (xhr.readyState == 4) {
-				            callback(xhr.response);
-				        }
-				    }
-				    xhr.send();
-				}
+				        if (xhr.readyState == 4) {callback(xhr.response);}}
+				    xhr.send();}
 				httpRequest("https://api.opendota.com/api/proPlayers/", function(result){
 				    team_list = JSON.parse(result);
 				    var teamArr = [];
 				    teamArr.push(team_list[0].team_id);
 				    var count = 0;
-				    for (var i=1;i<team_list.length;i++){
+				    for (var i=1,teamListLength=team_list.length;i<teamListLength;i++){
 				    		var k;
 				    		for (k=0;k<teamArr.length;k++){
 				    			if (teamArr[k]==team_list[i].team_id){
-				    				k --;
-				    				break;
-				    			}
-				    		}
+				    				k --;break;}}
 				    		if (k==teamArr.length){
-				    			count ++;
-				    			teamArr.push(team_list[i].team_id);
+				    			count ++;teamArr.push(team_list[i].team_id);
 				    			$('#selectTeam').editableSelect('add', '['+team_list[i].team_id+']'+team_list[i].team_name+'/'+team_list[i].team_tag);	
-				    		}else{
-				    			continue;
-				    		}
-					}
-				});
+				    		}else{continue;}} });
 				$('pre code').each(function(i, block) { hljs.highlightBlock(block); });
 				$.each(['basic', 'default', 'slide', 'fade', 'appendTo', 'no-filtering', 'html'], function (i, id) {
 					var $place  = $('#' + id + '-place');
 					var $select = $('#base').clone().removeAttr('id').appendTo($place);
 					if (id != 'basic') $select.find('option:selected').removeAttr('selected');
-					$select.editableSelect($place.data());
-				});
+					$select.editableSelect($place.data()); });
 				$('#selectTeam').editableSelect().on('select.editable-select', function (e, li) {				
 					var pattern = /\[(.*)\]/i;
 					var select_team_id = li.text().match(pattern)[1];
@@ -47,12 +34,9 @@ $(document).ready(function() {
 					localStorage.select_team_name = select_team_name;
 					localStorage.testfield = select_team_id;
 					var testteam = localStorage["testfield"];
-					if (localStorage["testfield"]){
-						$("#loadingTeamInfo").css('display','block');	
-					}				
+					if (localStorage["testfield"]){ $("#loadingTeamInfo").css('display','block');	}				
 					$("#team_member").css('display','none');
-					showMe(testteam,localStorage["select_team_name"]);	
-				});
+					showMe(testteam,localStorage["select_team_name"]);	});
 				$('#methods button').on('click', function () {
 					var action =  $(this).attr('id') || {
 						filter:   $('#filter').val() === 'True',
